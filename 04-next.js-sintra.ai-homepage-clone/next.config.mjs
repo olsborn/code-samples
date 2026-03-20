e88c0c867ff6107c686bfde77cfa8de03be204d0
olsborn <trojan@mnc.pl>
2026-03-20 18:11:17 +0100

    upd

--- 04-next.js-sintra.ai-homepage-clone/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    qualities: [85], // Add any other custom qualities you use
-    
-  },
-};
-
-export default nextConfig;
